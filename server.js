c8014e844994e9c09155d1ae6220f421dfd38151
kmietotwy <kmietotwy@gmail.com>
2019-04-18 10:45:30 -1000

    server side mvp working

--- server.js
+++ server.js
@@ -3,28 +3,65 @@ const net = require('net');
 const elements = require('./elements.js');
 
 let date = new Date();
-let errorBody = elements.error.body;
-let errorLength = elements.error.length;
 
-console.log(errorLength);
+let response = '';
 
-
-let response = `HTTP/1.1 200 OK
+let errorResponse = `HTTP/1.1 404 Page Not Found
 Date: ${date}
 Content-Type: text/html; charset=utf-8
-Content-Length: ${errorLength}
+Content-Length: ${elements.error.length}
 
-${errorBody}`;
+${elements.error.body}`;
+
+let htmlResponse = `HTTP/1.1 200 OK
+Date: ${date}
+Content-Type: text/html; charset=utf-8
+Content-Length: ${elements.index.length}
+
+${elements.index.body}`;
+
+let hydrogenResponse = `HTTP/1.1 200 OK
+Date: ${date}
+Content-Type: text/html; charset=utf-8
+Content-Length: ${elements.hydrogen.length}
+
+${elements.hydrogen.body}`;
+
+let heliumResponse = `HTTP/1.1 200 OK
+Date: ${date}
+Content-Type: text/html; charset=utf-8
+Content-Length: ${elements.helium.length}
+
+${elements.helium.body}`;
+
+let cssResponse = `HTTP/1.1 200 OK
+Date: ${date}
+Content-Type: text/html; charset=utf-8
+Content-Length: ${elements.css.length}
+
+${elements.css.body}`;
 
 // this creates a server
 const server = net.createServer((socket) => {
   socket.setEncoding('utf8');
   socket.on('data', (data) => {
     // this is the request
-    console.log('i dont know', data);
+    console.log(data);
 
     // do work here
-    
+    //interpret data being requested for path & craft responses accordingly
+    let first20CharactersOfData = data.substring(0, 20);
+  
+    if(first20CharactersOfData.indexOf('/index.html') !== -1 || first20CharactersOfData.indexOf('GET / HTTP') !== -1){
+      response = htmlResponse;
+    } else if(first20CharactersOfData.indexOf('/hydrogen.html') !== -1){
+      response = hydrogenResponse;
+    } else if(first20CharactersOfData.indexOf('/helium.html') !== -1){
+      response = heliumResponse;
+    } else if(first20CharactersOfData.indexOf('/css/styles.css') !== -1){
+      response = cssResponse;
+    } else { response = errorResponse;}
+
 
     // send response back here
     socket.end(response);
